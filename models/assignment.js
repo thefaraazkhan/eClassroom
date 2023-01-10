5963e8710c1d1e7a23e603ffecd46b4da8b62f1a
thefaraazkhan <khanmohdfaraaz@gmail.com>
2023-01-10 18:48:43 +0530

    formatted date

--- models/assignment.js
+++ models/assignment.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const assignmentSchema = new mongoose.Schema({
-    title: String,
-    details: String,
-    fileURL: String,
-    marks: Number,
-    dueDate: String,
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User'
-    },
-    createdAt: { 
-        type: Date,
-        default: Date.now()
-    },
+  title: String,
+  details: String,
+  fileURL: String,
+  marks: Number,
+  dueDate: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-module.exports = mongoose.model('Assignment', assignmentSchema);
+module.exports = mongoose.model("Assignment", assignmentSchema);
