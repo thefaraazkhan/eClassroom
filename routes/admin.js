d03c912c76dc62070eb532a7f192311fe5844f9c
thefaraazkhan <khanmohdfaraaz@gmail.com>
2023-01-11 18:49:26 +0530

    added feature: cannot submit after due date

--- routes/admin.js
+++ routes/admin.js
@@ -1,99 +1,103 @@
-const router = require('express').Router();
-const { response } = require('express');
-const passport = require('passport');
-const Assignment = require('../models/assignment');
-const User = require('../models/user');
-const multer = require('multer');
-const path = require('path');
-const CompletedAssignment = require('../models/completed-assignment');
+const router = require("express").Router();
+// const { response } = require('express');
+const passport = require("passport");
+const Assignment = require("../models/assignment");
+const User = require("../models/user");
+const multer = require("multer");
+const path = require("path");
+const CompletedAssignment = require("../models/completed-assignment");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/')
+    cb(null, "public/uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-  }
-})
+    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+  },
+});
 
 const upload = multer({ storage: storage });
 
-router.get('/addproject', (req, res)=>{
-    res.render("add-project")
+router.get("/addproject", (req, res) => {
+  res.render("add-project");
 });
 
-router.get('/projects', async (req, res)=>{
-  const assignments = await Assignment.find({createdBy: req.user._id});
+router.get("/projects", async (req, res) => {
+  const assignments = await Assignment.find({ createdBy: req.user._id });
   res.render("assigned-projects", {
     assignments,
   });
 });
 
-router.get("/submitted-projects", async (req, res)=>{
-  const assignments = await CompletedAssignment.find({createdBy: req.user._id}).populate('parentAssignment completedBy').exec();
+router.get("/submitted-projects", async (req, res) => {
+  const assignments = await CompletedAssignment.find({
+    createdBy: req.user._id,
+  })
+    .populate("parentAssignment completedBy")
+    .exec();
 
-  function formatDate(date){
+  function formatDate(date) {
     let d = new Date(date);
-    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`
+    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
   }
 
-  res.render('submitted-projects', {
+  res.render("submitted-projects", {
     assignments,
-    formatDate
+    formatDate,
   });
 });
 
-
-router.get('/project/:id', async (req, res)=>{
-  const assignment = await Assignment.findOne({_id: req.params.id});
-  let fileName = assignment.fileURL.split('/')[2];
-  res.render('t-project-detail', {
-      assignment,
-      fileName
+router.get("/project/:id", async (req, res) => {
+  const assignment = await Assignment.findOne({ _id: req.params.id });
+  function formatDate(date) {
+    let d = new Date(date);
+    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+  }
+  let fileName = assignment.fileURL.split("/")[2];
+  res.render("t-project-detail", {
+    assignment,
+    formatDate,
+    fileName,
   });
 });
 
-router.get('/deleteprojects', async (req, res)=>{
-  const assignments = await Assignment.find({createdBy: req.user._id});
+router.get("/deleteprojects", async (req, res) => {
+  const assignments = await Assignment.find({ createdBy: req.user._id });
   res.render("delete-projects", {
     assignments,
   });
 });
 
-
-router.get('/project/:id/delete', async (req, res)=>{
-  await Assignment.findOneAndDelete({_id: req.params.id});
-  await CompletedAssignment.findOneAndDelete({parentAssignment: req.params.id});
+router.get("/project/:id/delete", async (req, res) => {
+  await Assignment.findOneAndDelete({ _id: req.params.id });
+  await CompletedAssignment.findOneAndDelete({
+    parentAssignment: req.params.id,
+  });
   res.redirect("/");
 });
 
-
-router.post('/addproject', upload.single('file') ,async (req, res)=>{
-
-
-        await Assignment.create({
-            title: req.body.title,
-            details: req.body.detail,
-            fileURL: '/uploads/' + req.file.filename,
-            marks: Number(req.body.marks),
-            dueDate: new Date(req.body.due_date),
-            createdBy: req.user._id,
-        });
-        res.redirect('/dash');
-});
-
-
-
-router.post('/:id/givemarks', async (req, res)=>{
-
-  await CompletedAssignment.findOneAndUpdate({_id: req.params.id}, {
-    givenMarks: Number(req.body.marks)
+router.post("/addproject", upload.single("file"), async (req, res) => {
+  await Assignment.create({
+    title: req.body.title,
+    details: req.body.detail,
+    fileURL: "/uploads/" + req.file.filename,
+    marks: Number(req.body.marks),
+    dueDate: new Date(req.body.due_date),
+    createdBy: req.user._id,
   });
- 
-  res.redirect("/admin/submitted-projects");
-
+  res.redirect("/dash");
 });
 
+router.post("/:id/givemarks", async (req, res) => {
+  await CompletedAssignment.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      givenMarks: Number(req.body.marks),
+    }
+  );
+
+  res.redirect("/admin/submitted-projects");
+});
 
 // router.get("/assignment-details", async (req, res)=>{
 
--- routes/index.js
+++ routes/index.js
@@ -1,122 +1,129 @@
-const router = require('express').Router();
-const passport = require('passport');
-const User = require('../models/user');
-const Assignment = require('../models/assignment');
-const CompletedAssignment = require('../models/completed-assignment');
-const check = require('../middleware/check');
-const multer = require('multer');
-const path = require('path');
-const assignment = require('../models/assignment');
-const completedAssignment = require('../models/completed-assignment');
+const router = require("express").Router();
+const passport = require("passport");
+const User = require("../models/user");
+const Assignment = require("../models/assignment");
+const CompletedAssignment = require("../models/completed-assignment");
+const check = require("../middleware/check");
+const multer = require("multer");
+const path = require("path");
+const assignment = require("../models/assignment");
+const completedAssignment = require("../models/completed-assignment");
 const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/')
+    cb(null, "public/uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-  }
+    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+  },
 });
 
 const upload = multer({ storage: storage });
-router.get('/',check.isGuest ,(req, res)=>{
-    res.render('home');
+router.get("/", check.isGuest, (req, res) => {
+  res.render("home");
 });
 
-router.get('/register', check.isGuest ,(req, res)=>{
-    res.render('register');
+router.get("/register", check.isGuest, (req, res) => {
+  res.render("register");
 });
 
-router.get('/login', check.isGuest ,(req, res)=>{
-    res.render('login');
+router.get("/login", check.isGuest, (req, res) => {
+  res.render("login");
 });
 
-router.post('/register',(req, res)=>{
-    
-    const { firstName, lastName, username, password, type } = req.body;
-    const NewUser = new User({
-        username,
-        firstName,
-        lastName,
-        type
-    });
+router.post("/register", (req, res) => {
+  const { firstName, lastName, username, password, type } = req.body;
+  const NewUser = new User({
+    username,
+    firstName,
+    lastName,
+    type,
+  });
 
-    User.register(NewUser, req.body.password, (err, user)=>{
-        if(err){
-            console.log(err);
-            res.redirect('/register');
-            return;
-        }
-        res.redirect('/login');
-    });
-});
-
-
-router.post('/login', passport.authenticate('local', {
-    failureRedirect: '/login' 
-  }), (req, res)=>{
-      res.redirect('/dash');
-});
-
-router.get('/logout', (req, res)=>{
-      req.logOut();
-      req.logout();
-      res.redirect('/');
-});
-
-router.get('/dash', check.isLoggedin ,async (req, res)=>{
-
-    if(req.user.type === 'student'){
-        const completedAssignments = await CompletedAssignment.find({completedBy: req.user._id}).populate("parentAssignment").exec();
-        
-
-        let ids = completedAssignments.map((ca)=>{
-            return `${ca.parentAssignment._id}`;
-        });
-
-        let pendingAssignments = await Assignment.find({_id: {$nin: ids}});
-        res.render('dash', {
-            completedAssignments,
-            pendingAssignments
-        });
-    }else{
-        res.render('t-dash');
+  User.register(NewUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+      return;
     }
+    res.redirect("/login");
+  });
 });
 
-router.get('/aassignment/:id', async (req, res)=>{
-    const assignment = await Assignment.findOne({_id: req.params.id});
-    let fileName = assignment.fileURL.split('/')[2];
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    res.redirect("/dash");
+  }
+);
 
-    const completedAssignment = await CompletedAssignment.findOne({parentAssignment: assignment._id});
-    let isSubmitted = false;
-
-    if(completedAssignment){
-        isSubmitted = true;
-    }
-
-    res.render('assignment-details', {
-        assignment,
-        fileName,
-        isSubmitted,
-        completedAssignment
-    });
+router.get("/logout", (req, res) => {
+  req.logOut();
+  req.logout();
+  res.redirect("/");
 });
 
+router.get("/dash", check.isLoggedin, async (req, res) => {
+  if (req.user.type === "student") {
+    const completedAssignments = await CompletedAssignment.find({
+      completedBy: req.user._id,
+    })
+      .populate("parentAssignment")
+      .exec();
 
-router.post("/project/submit/:id", upload.single('file'), async (req, res)=>{
-
-    let teacherId = await Assignment.findOne({_id: req.params.id});
-
-    await CompletedAssignment.create({
-        parentAssignment: req.params.id,
-        createdBy: teacherId.createdBy,
-        completedFile: '/uploads/' + req.file.filename,
-        completedBy: req.user._id,
+    let ids = completedAssignments.map((ca) => {
+      return `${ca.parentAssignment._id}`;
     });
 
-    res.redirect('/dash');
+    let pendingAssignments = await Assignment.find({ _id: { $nin: ids } });
+    res.render("dash", {
+      completedAssignments,
+      pendingAssignments,
+    });
+  } else {
+    res.render("t-dash");
+  }
+});
+
+router.get("/aassignment/:id", async (req, res) => {
+  const assignment = await Assignment.findOne({ _id: req.params.id });
+  let fileName = assignment.fileURL.split("/")[2];
+  const completedAssignment = await CompletedAssignment.findOne({
+    parentAssignment: assignment._id,
+  });
+  let isSubmitted = false;
+
+  if (completedAssignment) {
+    isSubmitted = true;
+  }
+  function formatDate(date) {
+    let d = new Date(date);
+    return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+  }
+  res.render("assignment-details", {
+    assignment,
+    fileName,
+    formatDate,
+    isSubmitted,
+    completedAssignment,
+  });
+});
+
+router.post("/project/submit/:id", upload.single("file"), async (req, res) => {
+  let teacherId = await Assignment.findOne({ _id: req.params.id });
+
+  await CompletedAssignment.create({
+    parentAssignment: req.params.id,
+    createdBy: teacherId.createdBy,
+    completedFile: "/uploads/" + req.file.filename,
+    completedBy: req.user._id,
+  });
+
+  res.redirect("/dash");
 });
 
 module.exports = router;
