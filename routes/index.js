4b4dd76da341ce2614136d5437ffddb0c3f7d5b8
thefaraazkhan <khanmohdfaraaz@gmail.com>
2023-01-13 12:19:29 +0530

    feature: cloud upload for student files

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,47 @@ const assignment = require("../models/assignment");
 const completedAssignment = require("../models/completed-assignment");
 const mongoose = require("mongoose");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads/");
+// ***************************************************
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+// ***************************************************
+
+// ********** Cloud Upload ***********
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+
+// delete this: not needed
+// const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
-  },
+  region: bucketRegion,
 });
 
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+// ********** Cloud Upload ***********
+
 router.get("/", check.isGuest, (req, res) => {
   res.render("home");
 });
@@ -91,11 +122,15 @@ router.get("/dash", check.isLoggedin, async (req, res) => {
 
 router.get("/aassignment/:id", async (req, res) => {
   const assignment = await Assignment.findOne({ _id: req.params.id });
-  let fileName = assignment.fileURL.split("/")[2];
+
+  // console.log(assignment);
+  let fileName = assignment.fileURL.split("/")[1];
+
   const completedAssignment = await CompletedAssignment.findOne({
     parentAssignment: assignment._id,
   });
   let isSubmitted = false;
+  // console.log(completedAssignment);
 
   if (completedAssignment) {
     isSubmitted = true;
@@ -114,12 +149,36 @@ router.get("/aassignment/:id", async (req, res) => {
 });
 
 router.post("/project/submit/:id", upload.single("file"), async (req, res) => {
+  // console.log("req.body", req.body);
+  // console.log("req.file", req.file);
+
   let teacherId = await Assignment.findOne({ _id: req.params.id });
+
+  // ****************************************************************
+  const randomNum = Math.floor(Math.random() * 5000);
+  const fileName = req.file.originalname.split(" ").join("");
+  const randomImageName = `${randomNum}${req.user.firstName}-${fileName}`;
+  console.log(randomImageName);
+
+  // req.file.buffer is the actual image
+  const params = {
+    Bucket: bucketName,
+    Key: randomImageName,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype,
+  };
+
+  const command = await new PutObjectCommand(params);
+  s3.send(command);
+
+  const completedFileURL = "d31lyalmsd17xb.cloudfront.net/" + randomImageName;
+
+  // ****************************************************************
 
   await CompletedAssignment.create({
     parentAssignment: req.params.id,
     createdBy: teacherId.createdBy,
-    completedFile: "/uploads/" + req.file.filename,
+    completedFile: completedFileURL,
     completedBy: req.user._id,
   });
 
